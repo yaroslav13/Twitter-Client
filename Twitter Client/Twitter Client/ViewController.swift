29b59e9d280788b49571f1cf69c24c1723a456bb
yaroslav13 <37240325+yaroslav13@users.noreply.github.com>
2018-05-16 22:19:35 +0300

    Add files via upload

---
+++ Twitter Client/Twitter Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter Client
+//
+//  Created by Yaroslav Halivets on 5/16/18.
+//  Copyright © 2018 Yaroslav Halivets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
